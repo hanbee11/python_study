dc3f50c2096a491707b2e1f5835ebd0539d1a723
unknown <you@example.com>
2025-01-11 11:03:07 +0900

    first

---
+++ hello_world.py
@@ -0,0 +1,30 @@
+karl= "hello "
+malone= "world"
+print(karl+malone)
+
+print(len(karl))
+print(karl[-1])
+
+
+
+
+# len ----문자열의 길이
+# index -----문자열의 위치
+
+a='okc is the best team ever'
+print(a[0:30:2])
+print(a.count('e'))
+print(a.find('o'))
+name = 'karl malone'
+age = 61
+print(f'my name is {name}. my age is {age}.')
+
+a=",".join('supershai')
+print(a)
+
+
+
+
+
+
+
